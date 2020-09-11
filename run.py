440d7c6f493f54a4528779787431794ee5854631
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-09-11 01:56:58 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from base import create_app
 app = create_app()
 app.app_context().push()
 
-ENV= 'dev'
+ENV= 'prod'
 
 if ENV == 'dev':
     app.debug = True
@@ -11,4 +11,4 @@ else:
     app.debug = False
 
 if __name__ == '__main__':
-    app.run(host="127.1.1.4", debug=True)
+    app.run()
