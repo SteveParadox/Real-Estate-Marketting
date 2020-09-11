5d96fc05bfa37828503a9276eb6815538ab2315f
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-09-11 01:56:15 +0100

    Add files via upload

--- base/config.py
+++ base/config.py
@@ -2,13 +2,13 @@ import os
 
 
 class Config:
-    ENV = 'dev'
+    ENV = 'prod'
 
     if ENV == 'dev':
         SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     else:
 
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = 'postgres://xiwowberypopfh:164881e9c91553be395d82473917654c9083c308ec6400691e69cbbc669f6117@ec2-54-91-178-234.compute-1.amazonaws.com:5432/ddimctlrg827m2'
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SECURITY_PASSWORD_SALT = 'my_precious_two'
